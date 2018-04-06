3a4b130d34e019de2e9a6635df247326798159ce
AtreyeeS <atreyee.sinha@gmail.com>
2018-04-06 17:49:13 +0200

    first version of Mylikelihood

--- Mylikelihood.py
+++ Mylikelihood.py
@@ -3,5 +3,77 @@ class MyLikelihood(object):
     Class to compute the b_k and do the compute the likelihood function
     """
 
+    def __init__(self,image_list,diffuse_map,exclusion_mask=None):
+        self.count_list=[row[0] for row in image_list]
+        self.exposure_list=[row[1] for row in image_list]
+        self.background_list=[row[2] for row in image_list]
+        self.exclusion_mask=exclusion_mask
+        self.diffuse_map=diffuse_map
+
+
+
+    def __call__(self,alpha):
+        result=self.calc_res(alpha)
+        return result
+        
+
+    def calc_res(self,alpha):
+        #calculates the log likelihood function to minimise 
+        b = self.calc_bk(akpha)
+        res=0.0
+        for k in range(len(b)):
+            count_map=self.count_list[k]
+            exp_map=self.exposure_list[k]
+            bkg_map=self.background_list[k]
+            Nk=count_map.data * exp_map.data * self.exclusion_mask.data
+            Sk=self.diffuse_map.data * exp_map.data * self.exclusion_mask.data
+            Bk=bkg_map.data * self.exclusion_mask.data * exp_map.data #multiply with exposure?
+
+            tk=np.sum(Nk * np.log(alpha*Sk + b[k]*Bk)) - np.sum(alpha*Sk + b[k]*Bk)
+            res=res+tk
+        return res
+
+
+
+
+    def calc_bk(self,alpha):
+        bk1=[]
+        bk2=[]
+        for k in range(len(self.background_list)):
+            count_map=self.count_list[k]
+            exp_map=self.exposure_list[k]
+            bkg_map=self.background_list[k]
+            Nk=count_map.data * exp_map.data * self.exclusion_mask.data
+            Sk=self.diffuse_map.data * exp_map.data * self.exclusion_mask.data
+            Bk=bkg_map.data * self.exclusion_mask.data * exp_map.data #multiply with exposure?
+
+            not_has_exposure = ~(exp_map.data > 0)
+            not_has_bkg = ~(bkg_map.data > 0)
+            S_B=Sk/Bk
+            S_B[not_has_exposure]=0.0
+            S_B[not_has_bkg]=0.0
+
+            delta=np.power(np.sum(Nk)/np.sum(Bk),2.0) - 4.0 * alpha * np.sum(S_B)/np.sum(Bk)
+            if delta<0.0:
+                bk=999
+                continue
+
+            bk1=(np.sum(Nk)/np.sum(Bk) - np.sqrt(delta))/2.0
+            bk2=(np.sum(Nk)/np.sum(Bk) + np.sqrt(delta))/2.0
+            bk2.append(bk2)
+            bk1.append(bk1)
+        
+        return bk2
+
+
+
+            
+
+
+
+
+
+
+
     
 
---
+++ make_images.py
@@ -0,0 +1,70 @@
+"""
+to create the list of SkyImageLists
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import astropy
+import regions
+import sherpa
+import astropy.units as u
+from astropy.coordinates import SkyCoord, Angle
+from astropy.table import Table
+from regions import CircleSkyRegion
+from gammapy.data import DataStore
+from gammapy.image import SkyImage, IACTBasicImageEstimator, SkyImageList
+from gammapy.background import FoVBackgroundEstimator
+
+datastore = DataStore.from_dir("/Users/asinha/HESS_newbkg")
+name="LMC"
+src=SkyCoord.from_name(name)
+myid=np.loadtxt("LMC_id.txt")
+mylist=datastore.obs_list(myid)
+
+ref_image = SkyImage.empty(
+                           nxpix=400, nypix=400, binsz=0.05,
+                           xref=src.galactic.l.value, yref=src.galactic.b.value,
+                           coordsys='GAL', proj='TAN',
+                           )
+src_names=["LMC N132D","30 Dor C","LHA 120-N 157B","LMC P3"]
+l=[280.31,279.60, 279.55, 277.73]*u.deg
+b=[-32.78,-31.91, -31.75,-32.09 ]*u.deg
+radius=[0.2,0.2,0.5,0.2]*u.deg
+
+srctab=Table([src_names,l,b,radius],names=("src_names","longitude","latitude","radius"),meta={'name': 'known source'})
+
+off_regions=[]
+
+for s in srctab:
+    #circle=CircleSkyRegion(center=SkyCoord(s['longitude'], s['latitude'], unit='deg', frame='galactic'), radius=0.2*u.deg)
+    circle=CircleSkyRegion(center=SkyCoord(s['longitude'], s['latitude'], unit='deg', frame='galactic'), radius=s['radius']*u.deg)
+    off_regions.append(circle)
+
+exclusion_mask = ref_image.region_mask(off_regions[0])
+exclusion_mask.data = 1 - exclusion_mask.data
+for off in off_regions[1:]:
+    exclusion_mask.data -= ref_image.region_mask(off)
+
+
+exclusion_mask.data[exclusion_mask.data<0]=0   
+bkg_estimator = FoVBackgroundEstimator()
+
+
+emin=0.5 * u.TeV
+emax=20 * u.TeV
+offset_max=2.3 * u.deg
+
+image_estimator = IACTBasicImageEstimator(
+                                          reference=ref_image,
+                                          emin=emin,
+                                          emax=emax,
+                                          offset_max=offset_max,
+                                          background_estimator=bkg_estimator,
+                                          exclusion_mask=exclusion_mask)
+
+
+images=image_estimator.run_indiv(mylist)
+backnorm=[]
+for im in images:
+    backnorm.append(im["background"].meta['NORM'])
+
